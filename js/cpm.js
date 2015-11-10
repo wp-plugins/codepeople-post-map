abc6f10b538d2d4b1b69a8cf13ab3ddebe149713
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-10 11:55:33 +0000

    The current update removes the "sensor" parameter from the call to the Google API
    git-svn-id: https://plugins.svn.wordpress.org/codepeople-post-map/trunk@1283235 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/cpm.js
+++ js/cpm.js
@@ -345,7 +345,7 @@ function CodePeoplePostMapPublic()
 				// Create the script tag and load the maps api
 				var script=document.createElement('script');
 				script.type  = "text/javascript";
-				script.src=(( typeof window.location.protocol != 'undefined' ) ? window.location.protocol : 'http:' )+'//maps.google.com/maps/api/js?sensor=false'+((typeof cpm_language != 'undefined' && cpm_language.lng) ? '&language='+cpm_language.lng: '')+'&callback=cpm_init';
+				script.src=(( typeof window.location.protocol != 'undefined' ) ? window.location.protocol : 'http:' )+'//maps.google.com/maps/api/js?callback=cpm_init'+((typeof cpm_language != 'undefined' && cpm_language.lng) ? '&language='+cpm_language.lng: '');
 				document.body.appendChild(script);
 			}else{
 				cpm_init();
